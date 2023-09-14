c8ca27d20c52476fce62ef5df147fad80349141f
Awmin Codes <140441241+awmin89@users.noreply.github.com>
2023-09-15 00:09:15 +0330

    Beniko.py
    Coffee! 
     ~~~
      ____
    (       |
      ----'

--- Beniko.py
+++ Beniko.py
@@ -1,6 +1,6 @@
 import time
 
-time.sleep(5)
+time.sleep(3)
 #intro
 print('''
 
@@ -21,27 +21,27 @@ flavorings = ['Vanilla', 'Caramel', 'Hazelnut', 'Mocha', 'Nothing']
 
 sizes = ['Small', 'Medium', 'Large']
 
-#every prep time is different based on the coffee type!
-prep_times = [10, 25, 30, 26]
+
+prep_times = [10, 25, 30, 26]# Every prep time is different based on the coffee type!
 
 time.sleep(5.5)
-#Of course , input!
-input('[Enter] to continue')
-#Name of the user
+
+input('[Enter] to continue')# Of course , input!
+# Name of the user
 name = input('''----------------------------------
 So , What should i call you? ''')
-# if The User didn't written their name , Beniko will call them 'No one'
-if not name:
+
+if not name:# if The User's name is invalid , Beniko will call them 'No one'
     name = 'No one'
-#Coffee options
-for i, option in enumerate(coffee_options):
+
+for i, option in enumerate(coffee_options):# Coffee options
     print(f'{i+1}. {option}')
-#Order or Choices
+# Order or Choices
 choice =int(input('''
 ----------------------------------
 Alright ,Which coffee would you like to order? '''))
 print(f'Ok, {coffee_options[choice-1]}')
-#Advanced adjustment like Coffee bean or type of milk etc.
+# Advanced adjustment like Coffee bean or type of milk etc.
 advanced = input('''----------------------------------
 Do you want advanced adjustments (yup/nope)? 
 ''')
@@ -84,16 +84,14 @@ size_choice = int(input('''
 ----------------------------------
 Which size would you like? '''))
 print(f'Ok, {sizes[size_choice-1]}')
-#And here is extra shots of espresso option for when you want to get the full energy
+# And here is extra shots of espresso option for when you want to get the full energy
 extra_shots = int(input('''----------------------------------
 How many extra shots of espresso would you like (0-2)? '''))
-#Amount of everything before choosing
-sugar = -1
+sugar = -1# Amount of everything before choosing
 coffee = -1
 milk = -1
-#
-# Keep looping until the user enters valid values for sugar, coffee, and milk.
-while sugar < 0 or sugar > 5 or coffee < 0 or coffee > 5 or milk < 0 or milk > 5:
+
+while sugar < 0 or sugar > 5 or coffee < 0 or coffee > 5 or milk < 0 or milk > 5:# Keep looping until the user enters valid values for sugar, coffee, and milk.
     if sugar < 0 or sugar > 5:
         sugar = int(input("How many teaspoons of sugar would you like (0-5)? "))
     if coffee < 0 or coffee > 5:
@@ -106,41 +104,31 @@ while sugar < 0 or sugar > 5 or coffee < 0 or coffee > 5 or milk < 0 or milk > 5
         coffee = -1
         milk = -1
 
-# Print a message confirming the order.
-print(f"Preparing your {sugar} teaspoons of sugar, {coffee} shots of coffee, and {milk} ounces of milk...")
+print(f"Preparing your {sugar} teaspoons of sugar, {coffee} shots of coffee, and {milk} ounces of milk...")# Print a message confirming the order.
 
-# Sleep for a few seconds to simulate the time it takes to make the coffee.
-time.sleep(3)
+time.sleep(4)# Sleep for a few seconds to simulate the time it takes to make the coffee.
 
-# Print a message that the coffee is ready.
-print(f"Your coffee is ready, {name}!")
+print(f"Your coffee is ready, {name}!")# Print a message that the coffee is ready.
 
-# Check if the user ordered a sweet coffee.
 if sugar > 2:
-    print(f"Thank you for your purchase! Enjoy your really sweet cup of coffee!")
+    print(f"Thank you for your purchase! Enjoy your really sweet cup of coffee!")# Check if the user ordered a sweet coffee.
 
-# Check if the user ordered a strong coffee.
 elif coffee > 3:
-    print(f"Wow, {name}! You have a lot of caffeine in your coffee!")
+    print(f"Wow, {name}! You have a lot of caffeine in your coffee!")# Check if the user ordered a strong coffee.
 
-# Check if the user ordered a coffee with milk.
-elif milk > 0 and coffee_options[choice-1] in ['Espresso', 'Americano']:
+elif milk > 0 and coffee_options[choice-1] in ['Espresso', 'Americano']:# Check if the user ordered a coffee with milk.
     print(f"Are you sure you're old enough to drink coffee? Enjoy your coffee with milk!")
 
-# Check if the user ordered latte or cappuccino without milk.
-elif milk == 0 and coffee_options[choice-1] in ['Latte', 'Cappuccino']:
+elif milk == 0 and coffee_options[choice-1] in ['Latte', 'Cappuccino']:# Check if the user ordered latte or cappuccino without milk.
     print(f"We'll add some milk for you. It's delicious in a coffee!")
 
-# Otherwise, the coffee is just right.
-else:
+else:# Otherwise, the coffee is just right.
     print(f"Thank you for your purchase! Enjoy your delicious coffee!")
 
-# Get the user's feedback.
-feedback = input("What were your thoughts on this program? ")
+feedback = input("What were your thoughts on this program? ")# Get the user's feedback.
 
 # Write the feedback to a file. not important i just used it to anyone who used this program , the feedback get saved so i can review it later!
 with open("feedback.txt", "a") as f:
     f.write(feedback + "\n")
 
-# Thank the user for their feedback.
-print(f"Thank you for your feedback, {name}! Have a nice day!")
+print(f"Thank you for your feedback, {name}! Have a nice day!")# Thank the user for their feedback.
